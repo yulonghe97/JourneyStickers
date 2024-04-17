3866dad61e8a33352e0f1bf539844785361e5f40
0xYH <59072949+yulonghe97@users.noreply.github.com>
2024-04-16 22:51:59 -0400

    fixed theme toggle bug

--- package/src/components/ThemeToggle.tsx
+++ package/src/components/ThemeToggle.tsx
@@ -17,15 +17,14 @@ export function ThemeToggle() {
   const { setTheme } = useTheme()
 
   return (
+    <Animated>
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Animated>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
-        </Animated>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
@@ -39,5 +38,6 @@ export function ThemeToggle() {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
+    </Animated>
   )
 }
